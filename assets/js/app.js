38c541386b213258f0dd6027dd36eae9a1b888f9
Lateef Adeyekun <lateef.adeyekun@gmail.com>
2024-11-22 14:45:51 -0600

    added font awesome for the dices

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,23 @@ function select(selector, scope = document) {
 function listen(event, selector, callback) {
     return selector.addEventListener(event, callback);
 }
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const rollBtn = select('.roll-button');
+const rollArea = select('.area');
+const rollNumber = getRandomNumber(11, 66);
+const dice1 = select('.dice-one');
+const dice2 = select('.dice-two');
+
+listen('click', rollBtn, () => {
+    console.log('Dice roll');
+    roll();
+});
+
+function roll() {
+    let number = getRandomNumber(11, 66);
+    console.log(`Number is ${number}`);
+}

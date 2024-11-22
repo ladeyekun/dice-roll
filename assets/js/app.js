def70f51ed46e4767ad5c7bef7c32cc5447f02d1
Lateef Adeyekun <lateef.adeyekun@gmail.com>
2024-11-22 15:05:47 -0600

    roll dice working

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,8 +15,8 @@ function getRandomNumber(min, max) {
 const rollBtn = select('.roll-button');
 const rollArea = select('.area');
 const rollNumber = getRandomNumber(11, 66);
-const dice1 = select('.dice-one');
-const dice2 = select('.dice-two');
+const dice1Obj = select('.dice-one');
+const dice2Obj = select('.dice-two');
 
 listen('click', rollBtn, () => {
     console.log('Dice roll');
@@ -24,6 +24,38 @@ listen('click', rollBtn, () => {
 });
 
 function roll() {
-    let number = getRandomNumber(11, 66);
-    console.log(`Number is ${number}`);
+    let dice1 = getRandomNumber(1, 6);
+    let dice2 = getRandomNumber(1, 6);
+
+    dice1Obj.innerHTML = getFontAwesomeElement(dice1);
+    dice2Obj.innerHTML =getFontAwesomeElement(dice2);
+}
+
+function getDiceFont(dice) {
+    switch (dice) {
+        case 1:
+            return 'one';
+
+        case 2:
+            return 'two';
+
+        case 3:
+            return 'three';
+
+        case 4:
+            return 'four';
+
+        case 5:
+            return 'five';
+
+        case 6:
+            return 'six';
+    }
+}
+
+function getFontAwesomeElement(dice) {
+    let output = '';
+    output += `<i class="fa-solid fa-dice-${getDiceFont(dice)}"></i>`;
+
+    return output;
 }
